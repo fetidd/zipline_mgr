59346c1c1cc2564d36f95ac1fcfc900c4eaa3011
Ben Jones <fetiddius@gmail.com>
2025-05-13 09:10:31 +0100

    derp

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@ use std::io::Read;
 pub const BIND_ADDR_WINDS: &str = "127.0.0.1:8888";
 pub const BIND_ADDR_LINE: &str = "127.0.0.1:8889";
 
-
-
 pub struct Setup {
     black: bool,
     old_red_front: bool,
@@ -27,27 +25,27 @@ impl Message {
 
 impl std::fmt::Display for Message {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", match self {
-            Message::Weights(_, _, _, _) => "weights",
-            Message::ConfirmationRequest(_, _, _, _) => "confirm?",
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                Message::Weights(_, _, _, _) => "weights",
+                Message::ConfirmationRequest(_, _, _, _) => "confirm?",
+            }
+        )
     }
 }
-
-
 
 pub fn parse_message_type(raw: &Vec<u8>) -> Result<&fn(Vec<u8>) -> Message, CommunicationError> {
-    let raw_msg_type = String::from_utf8_lossy(&raw);
+    let raw_msg_type = String::from_utf8_lossy(&raw).into_owned();
     println!("{raw_msg_type}");
-    match raw_msg_type {
-        s if s == "0100" => Ok(&(Message::new_weights as fn(Vec<u8>) -> Message)),
-        _ => Err(CommunicationError::TODO)
+    match raw_msg_type.as_str() {
+        "0100" => Ok(&(Message::new_weights as fn(Vec<u8>) -> Message)),
+        _ => Err(CommunicationError::TODO),
     }
 }
-
 
 #[derive(Debug)]
 pub enum CommunicationError {
-    TODO
+    TODO,
 }
-
